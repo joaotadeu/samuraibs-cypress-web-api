3b1366b46c738d98088d09a866965a4c3536562b
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-20 19:24:06 -0300

    front-end

---
+++ tests/cypress/e2e/cadastro_samurai.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+describe('cadastro de cliente', () => {
+    it('deve cadastrar cliente com sucesso', () => {
+        const name = 'João Tadeu'
+        const email = 'joaotadeu1@gmail.com'
+        const password = '1234qwe'
+
+      cy.visit('http://localhost:3000/signup')
+      cy.title()
+            .should('eq', 'Samurai Barbershop by QAninja')
+
+      cy.get('input[placeholder="Nome"]').type(name)
+      cy.get('input[placeholder="E-mail"]').type(email)
+      cy.get('input[placeholder="Senha"]').type(password)
+
+      cy.contains('button[type="submit"]', 'Cadastrar').click()
+
+      cy.get('.toast')
+          .should('be.visible')
+          .find('p')
+          .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+    })
+
+    it('deve cadastrar cliente sem sucesso', () => {
+      const name = 'João Tadeu'
+      const email = 'joaotadeu@gmail.com'
+      const password = '1234qwe'
+
+    cy.visit('http://localhost:3000/signup')
+    cy.title()
+          .should('eq', 'Samurai Barbershop by QAninja')
+
+    cy.get('input[placeholder="Nome"]').type(name)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Senha"]').type(password)
+
+    cy.contains('button[type="submit"]', 'Cadastrar').click()
+
+    cy.get('.toast')
+        .should('be.visible')
+        .find('p')
+        .should('have.text', 'Email já cadastrado para outro usuário.')
+
+
+   
+  })
+
+  })
