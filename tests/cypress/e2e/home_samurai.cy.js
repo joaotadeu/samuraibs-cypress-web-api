f1ef3a5ac5a3b435158293636d695b741142cc20
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-20 18:32:22 -0300

    front-end

---
+++ tests/cypress/e2e/home_samurai.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('Webapp Status', () => {
+    it('deve estar online', () => {
+      cy.visit('http://localhost:3000/')
+      cy.title()
+            .should('eq', 'Samurai Barbershop by QAninja')
+    })
+  })
