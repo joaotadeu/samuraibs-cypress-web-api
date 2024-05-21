f692355a4bee378d803c5548b5f2b7e28de9f15e
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-21 19:40:28 -0300

    adicionando contexto visando a organização do projeto

--- tests/cypress/e2e/cadastro_samurai.cy.js
+++ tests/cypress/e2e/cadastro_samurai.cy.js
@@ -1,38 +1,42 @@
 /// <reference types="cypress" />
 
-describe('cadastro de cliente', () => {
+describe('Dado que cadastro de cliente', () => {
 
-      it('deve cadastrar cliente com sucesso', () => {
-
+      context('Quando o usuario é novo', () => {
             const user = {
                   name: ' João Tadeu S. Pereira',
                   email: 'joaotadeu@samurai.com',
                   password: '1234qwe',
             }
 
-            cy.task('removeUser', user.email)
-                  .then(function (result) {
-                        console.log(result)
-                  })
+            before(function () {
+                  cy.task('removeUser', user.email)
+                        .then(function (result) {
+                              console.log(result)
+                        })
+            })
 
-            cy.visit('http://localhost:3000/signup')
-            cy.title()
-                  .should('eq', 'Samurai Barbershop by QAninja')
+            it('então deve cadastrar cliente com sucesso', () => {
 
-            cy.get('input[placeholder="Nome"]').type(user.name)
-            cy.get('input[placeholder="E-mail"]').type(user.email)
-            cy.get('input[placeholder="Senha"]').type(user.password)
+                  cy.visit('http://localhost:3000/signup')
+                  cy.title()
+                        .should('eq', 'Samurai Barbershop by QAninja')
 
-            cy.contains('button[type="submit"]', 'Cadastrar').click()
+                  cy.get('input[placeholder="Nome"]').type(user.name)
+                  cy.get('input[placeholder="E-mail"]').type(user.email)
+                  cy.get('input[placeholder="Senha"]').type(user.password)
 
-            cy.get('.toast')
-                  .should('be.visible')
-                  .find('p')
-                  .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+                  cy.contains('button[type="submit"]', 'Cadastrar').click()
+
+                  cy.get('.toast')
+                        .should('be.visible')
+                        .find('p')
+                        .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+            })
+
       })
 
-      it('deve cadastrar cliente sem sucesso', () => {
-
+      context('Quando o usuario já existe', () => {
             const user = {
                   name: 'Diana Anjos',
                   email: 'diana@samurai1.com',
@@ -40,35 +44,40 @@ describe('cadastro de cliente', () => {
                   is_provider: true
             }
 
-            cy.task('removeUser', user.email)
-                  .then(function (result) {
-                        console.log(result)
-                  })
+            before(function () {
+                  cy.task('removeUser', user.email)
+                        .then(function (result) {
+                              console.log(result)
+                        })
 
-            cy.request(
-                  'POST',
-                  'http://localhost:3333/users',
-                  user
-            ).then(function (response) {
-                  expect(response.status).to.eq(200)
+                  cy.request(
+                        'POST',
+                        'http://localhost:3333/users',
+                        user
+                  ).then(function (response) {
+                        expect(response.status).to.eq(200)
+                  })
             })
 
-            cy.visit('http://localhost:3000/signup')
-            cy.title()
-                  .should('eq', 'Samurai Barbershop by QAninja')
-
-            cy.get('input[placeholder="Nome"]').type(user.name)
-            cy.get('input[placeholder="E-mail"]').type(user.email)
-            cy.get('input[placeholder="Senha"]').type(user.password)
-
-            cy.contains('button[type="submit"]', 'Cadastrar').click()
-
-            cy.get('.toast')
-                  .should('be.visible')
-                  .find('p')
-                  .should('have.text', 'Email já cadastrado para outro usuário.')
+            it('deve cadastrar cliente sem sucesso', () => {
 
 
+
+                  cy.visit('http://localhost:3000/signup')
+                  cy.title()
+                        .should('eq', 'Samurai Barbershop by QAninja')
+
+                  cy.get('input[placeholder="Nome"]').type(user.name)
+                  cy.get('input[placeholder="E-mail"]').type(user.email)
+                  cy.get('input[placeholder="Senha"]').type(user.password)
+
+                  cy.contains('button[type="submit"]', 'Cadastrar').click()
+
+                  cy.get('.toast')
+                        .should('be.visible')
+                        .find('p')
+                        .should('have.text', 'Email já cadastrado para outro usuário.')
+            })
 
       })
 
