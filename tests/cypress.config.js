892b6fb8ee16e283853057f3ae5ca1989401d71e
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-21 18:34:57 -0300

    adicionando conexão com o banco de dados

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,11 +1,31 @@
 const { defineConfig } = require("cypress");
+const { Pool } = require('pg');
 
 module.exports = defineConfig({
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const pool = new Pool({
+        host: 'localhost',
+        user: 'postgres',
+        password: '1234',
+        database: 'dbname',
+        port: 5432
+      });
+      
+      on('task', {
+        removeUser(email) {
+          return new Promise(function(resolve, reject) { // Adicionado reject para lidar com erros
+            pool.query('DELETE FROM public.users WHERE email = $1', [email], function(error, result) {
+              if (error) {
+                reject(error); // Rejeita a promise com o erro
+              }
+              resolve({ success: result.rowCount }); // Retorna o número de linhas afetadas
+            });
+          });
+        }
+      });
     },
   },
 });
--- tests/cypress/e2e/cadastro_samurai.cy.js
+++ tests/cypress/e2e/cadastro_samurai.cy.js
@@ -6,6 +6,11 @@ describe('cadastro de cliente', () => {
         const email = 'joaotadeu@gmail.com'
         const password = '1234qwe'
 
+      cy.task('removeUser', email)
+            .then(function(result){
+                  console.log(result)
+            })
+
       cy.visit('http://localhost:3000/signup')
       cy.title()
             .should('eq', 'Samurai Barbershop by QAninja')
@@ -14,16 +19,8 @@ describe('cadastro de cliente', () => {
       cy.get('input[placeholder="E-mail"]').type(email)
       cy.get('input[placeholder="Senha"]').type(password)
 
-      // cy.intercept('POST' ,'http://localhost:3333/users', {
-      //   statusCode: 200
-      // }).as('postUser')
-
-      
-
       cy.contains('button[type="submit"]', 'Cadastrar').click()
-
-      cy.wait('@postUser')
-
+      
       cy.get('.toast')
           .should('be.visible')
           .find('p')
