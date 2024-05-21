f9e12e10d90f35b50aa3244379b15e38f2a8c219
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-21 19:06:25 -0300

    adicionando conexão com o banco de dados

--- tests/cypress/e2e/cadastro_samurai.cy.js
+++ tests/cypress/e2e/cadastro_samurai.cy.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress" />
 
 describe('cadastro de cliente', () => {
-    it('deve cadastrar cliente com sucesso', () => {
-        const name = 'João Tadeu'
-        const email = 'joaotadeu@gmail.com'
-        const password = '1234qwe'
 
-      cy.task('removeUser', email)
+      const user = {
+            name: ' João Tadeu S. Pereira',
+            email: 'joaotadeu@gmail.com',
+            password: '1234qwe'
+      }
+
+    it('deve cadastrar cliente com sucesso', () => {
+      cy.task('removeUser', user.email)
             .then(function(result){
                   console.log(result)
             })
@@ -15,23 +18,19 @@ describe('cadastro de cliente', () => {
       cy.title()
             .should('eq', 'Samurai Barbershop by QAninja')
 
-      cy.get('input[placeholder="Nome"]').type(name)
-      cy.get('input[placeholder="E-mail"]').type(email)
-      cy.get('input[placeholder="Senha"]').type(password)
+      cy.get('input[placeholder="Nome"]').type(user.name)
+      cy.get('input[placeholder="E-mail"]').type(user.email)
+      cy.get('input[placeholder="Senha"]').type(user.password)
 
       cy.contains('button[type="submit"]', 'Cadastrar').click()
-      
+
       cy.get('.toast')
           .should('be.visible')
           .find('p')
-          .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+          .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
     })
 
     it('deve cadastrar cliente sem sucesso', () => {
-      const name = 'João Tadeu'
-      const email = 'joaotadeu@gmail.com'
-      const password = '1234qwe'
-
     cy.visit('http://localhost:3000/signup')
     cy.title()
           .should('eq', 'Samurai Barbershop by QAninja')
@@ -49,6 +48,6 @@ describe('cadastro de cliente', () => {
 
 
    
-  })
+    })
 
   })
