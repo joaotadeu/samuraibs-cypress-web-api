ecec8bab213f388fa3c2c7f461510d5859c16408
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-28 16:04:54 -0300

    implementando forma de externalizar massa de dados usando camadas do factories

--- tests/cypress/e2e/areaLogada.cy.js
+++ tests/cypress/e2e/areaLogada.cy.js
@@ -2,31 +2,18 @@
 
 import loginPage from '../support/pages/login'
 import areaLogada from '../support/pages/dash'
+import {cliente, barbeiro} from '../support/factories/dash'
 
 describe('Dado que o cliente está na area logada', function () {
 
     context('Quando o cliente faz um agendamento no App com sucesso', function () {
-        const massaDados = {
-            cliente: {
-                name: 'Joao Tadeu',
-                email: 'joaozinho@outlook.com',
-                password: '123qwe',
-                is_provider: false
-            },
-            barbeiro: {
-                name: 'Seu madruga',
-                email: 'ramon@televisa.com',
-                password: '123qwe',
-                is_provider: true
-            }
-        }
 
         before(function () {
-            cy.postUser(massaDados.barbeiro)
-            cy.postUser(massaDados.cliente)
+            cy.postUser(barbeiro)
+            cy.postUser(cliente)
 
-            cy.apiLogin(massaDados.cliente)
-            cy.setProviderId(massaDados.barbeiro.email)
+            cy.apiLogin(cliente)
+            cy.setProviderId(barbeiro.email)
 
             cy.log('ID do barbeiro é ' + Cypress.env('providerId'))
             cy.log('o Token é: ' + Cypress.env('apiToken'))
@@ -35,7 +22,7 @@ describe('Dado que o cliente está na area logada', function () {
 
         it('Então o mesmo deve ser exibido no dashboard', function () {
             loginPage.homePageLogin()
-            loginPage.formularioLogin(massaDados.barbeiro)
+            loginPage.formularioLogin(barbeiro)
             loginPage.logar()
 
             areaLogada.calendarioVisivel()
@@ -43,7 +30,7 @@ describe('Dado que o cliente está na area logada', function () {
             const dia = Cypress.env('appointmentDay') 
             areaLogada.diaSelecionado(dia)
 
-            areaLogada.apontamentoVisivel(massaDados.cliente.name)
+            areaLogada.apontamentoVisivel(cliente.name)
         })
     })
 
---
+++ tests/cypress/support/factories/dash/index.js
@@ -0,0 +1,14 @@
+exports.cliente = {
+    name: 'Joao Tadeu',
+    email: 'joaozinho@outlook.com',
+    password: '123qwe',
+    is_provider: false
+}
+
+exports.barbeiro = {
+    name: 'Seu madruga',
+    email: 'ramon@televisa.com',
+    password: '123qwe',
+    is_provider: true
+}
+
